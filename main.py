2051dceb9a29223d0a85338ad2ffd0bb5db57c96
Joss González <43999557+JossGonzalezDev@users.noreply.github.com>
2024-06-12 18:30:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-API_KEY = "sk_test_1234567890"
+def get_database_password():
+    password = "password123"
+    return password
+
+def connect_to_database():
+    db_password = get_database_password()
+    # Conectar a la base de datos usando la contraseña
+    print("Conectado a la base de datos con la contraseña:", db_password)
+
+if __name__ == "__main__":
+    connect_to_database()
